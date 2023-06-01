b9df12873619caf96e75ba1549e6fc0a026e7029
s0172067 <113241344+s0172067@users.noreply.github.com>
2023-06-01 05:10:41 +0300

    Update admin.php

--- 6lab/admin.php
+++ 6lab/admin.php
@@ -17,7 +17,7 @@ if($_SERVER['REQUEST_METHOD']=='GET'){
   if (empty($_SERVER['PHP_AUTH_USER']) ||
       empty($_SERVER['PHP_AUTH_PW']) ||
       $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-      md5($_SERVER['PHP_AUTH_PW']) != $pass_hash) {
+      md5($_SERVER['PHP_AUTH_PW']) !=  md5('123')) {
     header('HTTP/1.1 401 Unanthorized');
     header('WWW-Authenticate: Basic realm="My site"');
     print('<h1>401 Требуется авторизация</h1>');
